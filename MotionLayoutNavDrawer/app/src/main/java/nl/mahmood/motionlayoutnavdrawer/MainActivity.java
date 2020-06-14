cf92f7d20f9e98130a79742c2967af290ec37c50
Sayed Mahmood Ghaemmaghami <63294201+mahmood-ghaem@users.noreply.github.com>
2020-06-14 12:56:30 +0200

    add MotionLayoutNavDrawer

---
+++ MotionLayoutNavDrawer/app/src/main/java/nl/mahmood/motionlayoutnavdrawer/MainActivity.java
@@ -0,0 +1,25 @@
+package nl.mahmood.motionlayoutnavdrawer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+}
